678365d20a725c085670ca1be285a351a38e72b3
Belal Ahmad Abo Moailish <belalabomoalsh@gmail.com>
2023-09-30 07:03:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+const num1 = 6.2;
+const num2 = 10;
+let multiply = num1 * num2;
+
+const random = Math.round(Math.random() * 100 + 1);
+
+const num3 = 10;
+const num4 = 6;
+const mod = num3 % num4;
+
+const max = Math.max(1, 2, 5, 20);
